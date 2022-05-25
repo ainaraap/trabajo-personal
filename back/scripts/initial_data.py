861b371d1c91fd48b0b3e5d00994393dedda8167
ainara <ainaraap4@gmail.com>
2022-05-25 20:09:49 +0200

    BACK - cambios minimos en initial_data.py

--- back/scripts/initial_data.py
+++ back/scripts/initial_data.py
@@ -10,22 +10,19 @@ doll_repository = DollsRepository(database_path)
 
 
 muneca_1 = Doll(
-        doll_id= 1,
-        name= "Muñeca sentada",
-        price= "25.50 €",
-        img= "https://i.ibb.co/HrRB4ty/muneca1.png"
+    doll_id=1,
+    name="Muñeca sentada",
+    price="25.50 €",
+    img="https://i.ibb.co/HrRB4ty/muneca1.png",
 )
 muneca_2 = Doll(
-        doll_id=  2,
-        name= "Cojín",
-        price= "29.50 €",
-        img= "https://i.ibb.co/DpKtm2H/muneca2.png"
+    doll_id=2, name="Cojín", price="29.50 €", img="https://i.ibb.co/DpKtm2H/muneca2.png"
 )
 muneca_3 = Doll(
-        doll_id=  3,
-        name= "Muñeca portapapel WC ",
-        price= "30 €",
-        img= "https://i.ibb.co/n6hG04G/muneca3.jpg"
+    doll_id=3,
+    name="Muñeca portapapel WC ",
+    price="30 €",
+    img="https://i.ibb.co/n6hG04G/muneca3.jpg",
 )
 
 dolls_repository = DollsRepository(database_path)
@@ -34,6 +31,3 @@ dolls_repository.save(muneca_2)
 dolls_repository.save(muneca_3)
 
 print("Base de datos creada")
-
-
-
